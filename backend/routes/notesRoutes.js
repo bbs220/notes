e79f4d79a4f2c69f760cf5b805fdc12e5348536a
bbs220 <bsawant357@gmail.com>
2024-06-19 20:11:24 +0530

    post get getId done

--- backend/routes/notesRoutes.js
+++ backend/routes/notesRoutes.js
@@ -1,5 +1,57 @@
 import express from "express";
+import notesModel from "../models/notesModel.js";
 
 const router = express.Router();
+
+router.post("/notes", async (req, res) => {
+  try {
+    if (!req.body.title || !req.body.content) {
+      return res.status(400).json({
+        message: "Need both title and content for a note",
+      });
+    }
+
+    const newNote = {
+      title: req.body.title,
+      content: req.body.content,
+    };
+
+    const note = await notesModel.create(newNote);
+
+    console.log("New note created ✅");
+
+    res.status(200).json(note);
+  } catch (error) {
+    console.log("Error occured : ", error);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.get("/notes", async (req, res) => {
+  try {
+    const allNotes = await notesModel.find({});
+
+    res.status(200).json({
+      count: allNotes.length,
+      data: allNotes,
+    });
+  } catch (error) {
+    console.log("Error occured : ", error);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.get("/notes/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const noteId = await notesModel.findById(id);
+
+    res.status(200).json(noteId);
+  } catch (error) {
+    console.log("Error occured : ", error);
+    res.status(500).json({ message: error.message });
+  }
+});
 
 export default router;
