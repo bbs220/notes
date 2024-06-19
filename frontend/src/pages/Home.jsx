3c338903d0f838b551cde54512ee3df4a7401fae
bbs220 <bsawant357@gmail.com>
2024-06-19 22:02:34 +0530

    search bar

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import Footer from "../components/Footer";
 import Gridbox from "../components/GridBox/Gridbox";
 import Navbar from "../components/NavBar/Navbar";
+import SearchBar from "../components/SearchBar";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <div className="divider"></div>
+      <SearchBar />
       <div className="divider"></div>
       <Gridbox />
       <div className="divider"></div>
