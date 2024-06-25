8dc8c6a218c9f79e05312b67ce195f0b79bfb762
bbs220 <bsawant357@gmail.com>
2024-06-25 13:37:51 +0530

    improved error bg

--- frontend/src/components/Background/ServerError.jsx
+++ frontend/src/components/Background/ServerError.jsx
@@ -4,9 +4,9 @@ const ServerError = () => {
   return (
     <div className="w-full flex justify-center items-center">
       <div className="p-4 w-full md:w-1/2 mx-4 md:mx-0 min-h-96 rounded-2xl bg-error flex flex-col gap-y-2 justify-center items-center">
-        <div className="flex items-center justify-center gap-x-4">
+        <div className="flex items-center justify-center gap-x-4 flex-col md:flex-row">
           <BiSolidErrorAlt className="text-3xl" />
-          <p className="font-bold text-3xl">An error occurred</p>
+          <p className="font-bold text-3xl mt-4 md:mt-0">An error occurred</p>
         </div>
         <p className="text-2xl">Please try again later.</p>
       </div>
